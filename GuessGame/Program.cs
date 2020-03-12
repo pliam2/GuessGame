8f7b5b4de0f014005e97b3a6f6f9986ad3668bc5
pliam2 <62082834+pliam2@users.noreply.github.com>
2020-03-11 22:16:58 -0230

    Add files via upload

---
+++ GuessGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guess;
+            int answer = 42;
+
+            Console.Write("Enter a number between 1 and 100 to guess the answer: ");
+
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            while (guess != answer)
+            {
+
+                if (guess < 1 || guess > 100)
+                {
+                    Console.WriteLine("Please enter a number between 1 and 100");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect");
+                }
+
+                Console.Write("Enter a number between 1 and 100 to guess the answer: ");
+
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == answer)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Correct");
+
+        }
+    }
+}
